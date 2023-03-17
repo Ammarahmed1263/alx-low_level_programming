df702e3cf6ea735c8c0d478b5042285fdc81a405
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-16 21:12:00 -0700

    pass all

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print to stderr
+ *
+ * Return: 1 always
+ */
+int main()
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
