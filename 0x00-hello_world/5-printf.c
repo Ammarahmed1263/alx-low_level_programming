d14a16ce104af41f8a8bddd2b46d8e09aab37152
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-16 19:54:17 -0700

    pass hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints sentence
+ *
+ * Return: 0 after execution done
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
