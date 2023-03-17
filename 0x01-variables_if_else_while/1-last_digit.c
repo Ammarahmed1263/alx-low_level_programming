3c40129ae46c86dbb135e9d06049a8351574b176
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-17 07:23:50 -0700

    pass 0,1,2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,10 +19,10 @@ int main(void)
 
 	n_mod = n % 10;
 	if (n_mod > 5)
-		printf("Last digit of %i is %i and is greater than 5", n, n_mod);
+		printf("Last digit of %i is %i and is greater than 5\n", n, n_mod);
 	else if (n_mod == 0)
-		printf("Last digit of %i is %i and is 0", n, n_mod);
+		printf("Last digit of %i is %i and is 0\n", n, n_mod);
 	else if (n_mod < 6 && n_mod != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0", n, n_mod);
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n_mod);
 	return (0);
 }
