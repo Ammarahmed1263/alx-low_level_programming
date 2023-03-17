1aefa8c8146e0322c00811f375f9180877707f07
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-17 05:35:14 -0700

    first alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
