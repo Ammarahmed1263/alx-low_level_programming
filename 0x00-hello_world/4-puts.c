414e073b466a9c2db34a5cd6235587e1b1bf3fcd
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-16 19:51:15 -0700

    puts modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - prints sentence
+ * main - prints sentence
+ *
+ * Return: 0 after execution done
  */
 
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
