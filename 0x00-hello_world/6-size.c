d559938ce531d225c7766bf0df3912195189e093
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-16 21:27:44 -0700

    update 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - print sizes
+ *
+ * Return: 0 always
+ */
 int main(void)
 {
 	printf("Size of a char: %li byte(s)\n", (unsigned long)sizeof(char));
@@ -7,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %li byte(s)\n", (unsigned long)sizeof(long int));
 	printf("Size of a long long int: %li byte(s)\n", (unsigned long)sizeof(long long int));
 	printf("Size of a float: %li byte(s)\n", (unsigned long)sizeof(float));
+	return (0);
 }
