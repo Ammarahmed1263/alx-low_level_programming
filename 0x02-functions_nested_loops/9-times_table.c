7917acc10b609fa680672d008fe767bb348c3d1a
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-21 19:04:48 -0700

    pass left v.4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,13 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for(j = 0; j < 9; j++)
+		for (j = 0; j < 9; j++)
+		{
 			if (i * (j + 1) / 10 == 0)
 				printf("%i,  ", i * j);
 			else
 				printf("%i, ", i * j);
+		}
 		printf("%i\n", 9 * i);
 	}
-		
 }
