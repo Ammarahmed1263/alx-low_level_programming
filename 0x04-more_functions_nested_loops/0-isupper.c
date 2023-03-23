7574d4176512fd2df95ef4cc192a850cbc6f6513
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-23 14:58:07 -0700

    pass upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - check if upper alpha
+ *@c: alpha to be checked
+ *
+ * Return: 1 if upper else 0
+ */
+
+int _islower(int c)
+{
+	int i, counter = 0;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		if (i == c)
+			counter++;
+	}
+
+	if (counter != 0)
+		return (1);
+	return (0);
+}
