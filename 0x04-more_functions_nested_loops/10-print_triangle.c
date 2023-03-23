2abe783cd7971523403f5d29e9b6b3557be9545e
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-23 13:01:50 -0700

    pass triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle of hash
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if ( size > 0)
+	{
+        	for (int i = 0; i < size; i++)
+        	{
+        	        for (int j = 0; j < size - i; j++)
+	                        _putchar(' ');
+
+        	        for (int k = 0; k < i + 1; k++)
+	                        _putchar('#');
+
+                	_putchar('\n');
+        	}
+	}
+	else
+		_putchar('\n');
+}
