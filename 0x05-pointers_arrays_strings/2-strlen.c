cb307045af79198a9fc34231c0eff73ca59dec2f
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-27 15:12:08 -0700

    pass 1,2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - prints length of string
+ * @s: pointer to string
+ *
+ * Return: number of characters
+ */
+
+int _strlen(char *s)
+{
+	char *i;
+	int counter = 0;
+
+	for(i = s; *i != '\0'; i++)
+		counter++;
+
+	return (counter);
+}
