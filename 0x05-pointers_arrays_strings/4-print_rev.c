c4390f342671d476236cd97aa9e6d2fef0dedc6b
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-27 16:00:38 -0700

    pass four

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints reverse of string
+ * @s: pointer to string
+ *
+ */
+
+void print_rev(char *s)
+{
+	char *i;
+
+	for (i = s; *i != '\0'; i++)
+		continue;
+
+	while (i >= s)
+	{
+		_putchar(*i);
+		i--;
+	}
+
+	_putchar('\n');
+}
