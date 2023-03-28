9b5239a5ebc574e0297bf9f32a551d3adc8631c3
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-27 17:25:49 -0700

    pass 6.6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,13 @@ void puts2(char *str)
 {
 	char *i;
 
-	for (i = str + 1; *i != '\0'; i += 2)
-		_putchar(*(i - 1));
+	for (i = str; *i != '\0'; i += 2)
+	{
+		_putchar(*i);
+
+		if (i++ == '\0')
+			break;
+	}
 
 	_putchar('\n');
 }
