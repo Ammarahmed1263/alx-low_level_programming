be5185f17c4e6843131035379c36b058f3d8c9ad
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-27 19:18:55 -0700

    pass seven

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints second half
+ * @str: pointer to string
+ *
+ */
+
+void puts_half(char *str)
+{
+	char *i;
+	int len = 0, j, n;
+
+	for (i = str; *i != '\0'; i++)
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		n = (len - 1) / 2;
+
+	for (j = n; str[j] != '\0'; j++)
+		_putchar(str[j]);
+
+	_putchar('\n');
+}
