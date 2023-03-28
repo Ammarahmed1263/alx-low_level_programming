b121fd43bc49d5ed61e67a7858d3ec0f6c677bec
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-27 17:48:25 -0700

    pass 8.2

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%i", a[i]);
 
-		if (i < n - 1)
-			printf(", ");
+		for (i = 0; i < n; i++)
+		{
+			printf("%i", a[i]);
+
+			if (i < n - 1)
+				printf(", ");
+		}
+		printf("\n");
 	}
 }
