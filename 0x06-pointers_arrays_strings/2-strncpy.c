b36d318f651439490c92c054ae7d854d1b380f54
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-29 16:30:47 -0700

    pass two

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _stnrcpy - copy src into des for n bytes
+ * @dest: where to copy
+ * @src: from where to copy
+ * @n: number of bytes
+ *
+ * Return: pointer to new string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
