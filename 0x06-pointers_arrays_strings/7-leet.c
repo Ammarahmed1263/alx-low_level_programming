cea9a54b9f3446e46f4f34317905bedd959e90ee
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-30 15:43:05 -0700

    pass seven.2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,16 +10,16 @@ char *leet(char *str)
 {
 	int i, j;
 
-	char s1[] = "aAeEoOtTlL"
-	char s2[] = "4433007711"
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (s1[j] == str[j])
+			if (s1[j] == str[i])
 			{
-				str[j] = s2[j];
+				str[i] = s2[j];
 				break;
 			}
 		}
