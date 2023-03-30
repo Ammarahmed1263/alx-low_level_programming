c11d201542ad19901b9cccd988bf8a6b2e7cffb4
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-29 17:22:44 -0700

    pass four

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverses given array
+ * @a: pointer to array
+ * @n: array size
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp, j;
+	
+	for (j = 0; j < n / 2; j++)
+	{
+		temp = s[j];
+		s[j] = s[n - 1 - j];
+		s[n - 1 - j] = temp;
+	}
+}
