9e31c2ff302d523f9c978cb47097cfdd234acff8
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-03-30 15:36:39 -0700

    pass seven

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - change specific letters
+ * @str: string to edit
+ *
+ * Return: edited string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	char s1[] = "aAeEoOtTlL"
+	char s2[] = "4433007711"
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s1[j] == str[j])
+			{
+				str[j] = s2[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
