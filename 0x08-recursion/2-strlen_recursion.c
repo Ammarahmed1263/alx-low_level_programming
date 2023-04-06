10e16549b79cd5617411b9346e3ba370b1445eb3
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-05 19:45:28 -0700

    pass two

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - prints length of string
+ * @s: string to calculate its length
+ *
+ * Return: 0 if end of string 1 + smaller
+ * problem other wise
+ */
+int _strlen_recursion(char* s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
