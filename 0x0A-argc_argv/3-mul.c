012aab0c440f6fc42c57d929a181e0fac283aba3
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-09 14:51:43 -0700

    pass three

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints sum of arguments
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+		printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
