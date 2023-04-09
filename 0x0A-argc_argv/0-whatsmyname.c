5e837740a0f8d2deccfbcba5ac3df8b276731361
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-09 13:15:32 -0700

    pass one

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 /**
- * main - prints second argument name
+ * main - prints first argument name
  * @argc: arguments count
  * @argv: arguments vector
  *
- * Return: string for second argument
+ * Return: always 0
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints arguments count
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0 always
+ */
+
+int main(int argc, char *argv[]  __attribute__((unused)))
+{
+	printf("%i\n", argc);
+	return (0);
+}
