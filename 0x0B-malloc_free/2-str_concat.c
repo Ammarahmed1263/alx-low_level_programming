a33d7c206d1f03172a6f258c548e94c09fc5f43a
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-11 21:38:44 -0700

    final pass

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,7 +26,7 @@ char *str_concat(char *s1, char *s2)
 	else if (s1 == NULL)
 		s1 = "";
 	else if (s2 == NULL)
-		s1 = "";
+		s2 = "";
 
 	pt = (char *)malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 
