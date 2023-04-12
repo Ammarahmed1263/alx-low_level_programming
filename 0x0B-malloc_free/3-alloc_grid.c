69f863a3f0dbd74dd539f97d3a3062b7ca05ba36
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-12 09:44:31 -0700

    pass three

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * alloc_grid - creates grid
+ * @width: number of columns
+ * @height: number of rows
+ *
+ * Return: NULL on failure
+ * or width, height equals zero
+ * else return pointer to grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **ptt;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	ptt = (int **)malloc(height * sizeof(int));
+
+	if (ptt == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptt[i] = (int *)malloc(width * sizeof(int));
+	}
+
+	/* ptt[i][j] same as *(*(ptt + i) + j) */
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			*(*(ptt + i) + j) = 0;
+	return (ptt);
+}
