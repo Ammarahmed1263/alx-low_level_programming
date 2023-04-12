7286bce4c3dd59fea34bc1e15048d88a491d3de2
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-11 20:25:15 -0700

    pass 2.4.3

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 	char *pt;
 
 	if (s1 == NULL && s2 == NULL)
-		return (strdup(""));
+		return (strdup(" "));
 	else if (s1 == NULL)
 		return (s2);
 	else if (s2 == NULL)
