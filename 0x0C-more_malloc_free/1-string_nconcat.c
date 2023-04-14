1dd4c2f4a46cd62adf3dbc4f4bc1969d6c64f16e
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-13 22:25:45 -0700

    pass one.2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,11 +14,12 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j,s1_len = strlen(s1),s2_len = strlen(s2);
+	unsigned int i, j, s1_len = strlen(s1), s2_len = strlen(s2);
 	char *ptr;
-	s1 = s1?s1:NULL;
-	s2 = s2?s2:NULL;
-	
+
+	s1 = s1 ? s1 : "";
+	s2 = s2 ? s2 : "";
+
 	if (n >= s2_len)
 	{
 		ptr = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
@@ -31,12 +32,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0';i++)
+	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-	
-	for (j = 0; s2[j] != '\0' && j < n;j++)
+
+	for (j = 0; s2[j] != '\0' && j < n; j++)
 		ptr[i++] = s2[j];
-	
+
 	ptr[i] = '\0';
-	return ptr;
+	return (ptr);
 }
