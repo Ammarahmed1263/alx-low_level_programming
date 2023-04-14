271fc393abac0ca4fdf7acb356e5430f7a483f7c
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-14 11:54:58 -0700

    pass two

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _calloc - alocats and intializes block of memory
+ * @nmemb: number of elements in array
+ * @size: size of each element
+ *
+ * Return: null if size is 0 or it fails
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (!ptr)
+		return (NULL);
+
+	for(i = 0; i < nmemb; i++)
+		*((char *)ptr + i) = 0;
+	
+	return (ptr);
+}
