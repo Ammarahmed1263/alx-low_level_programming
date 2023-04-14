ff05d0973b28b4e1db70c7571e419a010ac21445
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-13 19:50:18 -0700

    pass three

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - returns array in given range
+ * @min: first int
+ * @max: last int
+ *
+ * Return: in success pointer to allocated array
+ * at fail or min > max returns null
+ */
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i;
+
+	ptr = (int *)malloc(sizeof(int) * (max - min + 1));
+
+	if (min > max || !ptr)
+		return (NULL);
+
+	for (i = 0; i < max - min; i++)
+		ptr[i] = min + i;
+
+	return (ptr);
+}
