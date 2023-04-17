7d33619f66ae078ed0ffc37d8461f5084313e344
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-17 10:36:36 -0700

    pass one

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * init_dog - fill dog's data
+ * @d: struct of type dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
