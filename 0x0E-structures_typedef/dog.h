ed2d936ecbbc98c4effdcc795311c3041637ca0d
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-17 09:51:39 -0700

    pass zero

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - specifies dog characteristics
+ * @name: dog name
+ * @owner: name of dog owner
+ * @age: dog age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
