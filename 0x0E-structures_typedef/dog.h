5c8a28880778f6bf9550136cb9fe4b2467434599
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-17 13:59:06 -0700

    pass 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - specifies dog characteristics
+ * dog_t - specifies dog characteristics
  * @name: dog name
  * @owner: name of dog owner
  * @age: dog age
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-};
+}dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
