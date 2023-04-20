8711d8ae8589296c38deb0827b27ff01fec423ae
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-19 19:27:31 -0700

    pass one

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - applies function on all array elements
+ * @array: array to be iterated
+ * @size: size of array
+ * @action: function to apply
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action || !size)
+		return;
+
+	for (i = 0; i < 5; i++)
+		action(array[i]);
+}
