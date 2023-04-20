949253ba790a50448489db8b297b5e6c1127c9f4
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-04-19 19:38:39 -0700

    pass one

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,9 +9,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (!array || !action || size <= 0)
+	if (!array || !action)
 		return;
 
-	for (i = 0; i < 5; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
