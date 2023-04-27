265ff602fc9d15e94de9dd97e78130d758106f5b
AmmarAhmed <ammarahmed1263@gmail.com>
2023-04-27 15:08:19 -0700

    pass one.2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * print_list - prints all list elements
+ * print_len - prints list elements count
  * @h: pointer to the list
  *
  * Return: the number of nodes
  */
 
-size_t print_list(const list_t *h)
+size_t print_len(const list_t *h)
 {
 	int count = 0;
 
