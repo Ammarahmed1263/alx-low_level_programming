4717b66b1a3dcb1faca62909c172a5e150f53071
AmmarAhmed <ammarahmed1263@gmail.com>
2023-04-27 19:30:56 -0700

    pass four

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,15 @@
 #include "lists.h"
 /**
- * ffree_list - frees linked list
+ * free_list - frees linked list
  * @head: pointer to the list
  *
  */
 void free_list(list_t *head)
 {
-	list_t *trav;
-
 	if (!head)
 		return;
 
-	trav = head;
-	free(trav);
-	head = head->next;
+	free_list(head->next);
+	free(head);
 
-	free_list(head);
 }
