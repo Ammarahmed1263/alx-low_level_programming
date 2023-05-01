d02f40970f7e783e922327c05d04393851eb29d5
AmmarAhmed <ammarahmed1263@gmail.com>
2023-05-01 03:06:50 -0700

    pass two

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_nodeint - add node to linked list
+ * @head: the head pointer to the list
+ * @n:data included in the node
+ *
+ * Return: on success pointer to list after modification
+ * and on fail returns null
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+        listint_t *curr;
+
+        curr = malloc(sizeof(listint_t));
+
+        if (!curr)
+                return (NULL);
+
+        curr->n = n;
+        curr->next = *head;
+        *head = curr;
+
+}
