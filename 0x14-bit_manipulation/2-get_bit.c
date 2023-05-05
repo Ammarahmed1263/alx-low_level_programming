bcf9fbe568b7895760c001c7263608a55255caf3
AmmarAhmed <ammarahmed1263@gmail.com>
2023-05-04 19:55:55 -0700

    pass two

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * get_bit - return value of bit at given index
+ * @n: input number
+ * @index: place of bit
+ *
+ * Return: value of bit on succes and -1 on error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int rem;
+
+	if (index > 63)
+		return (-1);
+
+	return ((n >> index) & 1);
+}
