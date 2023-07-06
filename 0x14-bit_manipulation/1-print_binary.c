1d0fade75267467d304e4c30cd521f5f623ec638
Ammarahmed1263 <ammarahmed1263@gmail.com>
2023-07-06 16:09:38 -0700

    fix betty for two

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,21 +4,19 @@
  * print_binary - prints binary of decimal
  * @n: decimal to be converted
  *
- * Description: this function takes an 
+ * Description: this function takes an
  * integer and returns its binary representation
  */
-int counter = 0;
 void print_binary(unsigned long int n)
 {
 	if (!n)
 	{
-		if (!counter)
-			_putchar('0');
-
+		_putchar('0');
 		return;
-	
 	}
-	counter++;
-	print_binary(n >> 1);
+
+	if (n > 1)
+		print_binary(n >> 1);
+
 	_putchar((n & 1) + '0');
 }
