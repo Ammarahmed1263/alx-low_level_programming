6a0319eada40b09727fe55e5ff9705635180f864
Ammarahmed1263 <ammarahmed1263@gmail.com>
2023-07-06 18:41:37 -0700

    pass set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - finds bit at nth position
+ * @n: input decimal to search
+ * @index: index of required digit
+ *
+ * Description: this functions sets
+ * bit value at given index from the given
+ * integer
+ *
+ * Return: 1 on success or -1 on error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+
+	return (*n |= (1 << index));
+}
