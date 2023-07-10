459fd90d3ca1955a96a1e6dbb494efc653a69198
Ammarahmed1263 <ammarahmed1263@gmail.com>
2023-07-10 14:15:42 -0700

    first commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads file and print it to stdout
+ * @filename: name of file to read from
+ * @letters: number of letter should be read
+ *
+ * Description: this function reads a text
+ * file and prints it to the POSIX standard output
+ *
+ * Return: number of letters read on success
+ * and 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, wtn, rd;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY | O_CREAT);
+
+	if (fd == -1)
+		return (0);
+	
+	buf = (char *)malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+		
+	rd = read(fd, buf, letters);
+	if (rd < 0)
+		return (0);
+
+	wtn = write(STDOUT_FILENO, buf, letters);
+	if (wtn < 0)
+		return (0);
+	
+	close(fd);
+	free(buf);
+	return (wtn);
+}
