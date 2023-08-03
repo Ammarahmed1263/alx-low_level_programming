7f392b46141f7b33be32a5bb3ad75706bb5d5671
ammarahmed1263 <ammarahmed1263@gmail.com>
2023-08-03 09:38:28 -0700

    modify docs 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -5,9 +5,11 @@
  *
  * Description: bytes order is stored according to
  * system architecture so value of 1 will be stored
- * either on left most bits "first 1 bytes" in big
- * endianness or on right most bits for little endianness
- * and casting will take only only right part of address
+ * either on left most bits "first 1 bytes" in big -
+ * end or on right most bits for little-end
+ * and casting will take only only left part of address
+ * which in case of big-end will be most significant byte
+ * while in case of little-end will be least significant byte
  *
  * Return: 0 if big endian, 1 if little endian
  */
